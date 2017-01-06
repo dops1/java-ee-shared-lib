340acedd8fda8f80cec71cc05b8bf129e1041610
Zhelyan <zhelyan.panchev@citizensadvice.org.uk>
2017-01-06 18:11:02 +0000

    more typos

--- vars/performanceTest.groovy
+++ vars/performanceTest.groovy
@@ -1,5 +1,11 @@
-def call(){
-	echo "Testing ${config.endpoint}"
+def call(Map config){
+
+   if(config.endpoint == null){
+	 error "endpoint: <URL> is required!"
+   }
+
+   echo "Testing ${config.endpoint}"
+   
    for (i = 0; i < 5; i++) { 
         println "Requests: " +  i**(i) 
         sleep i
